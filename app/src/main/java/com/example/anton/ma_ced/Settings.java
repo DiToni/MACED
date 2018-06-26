f13e58cfa268a3c6f9a50fe6f94d50fb1f413d77
DiToni <Ad1991Ha>
2018-06-26 11:45:46 +0200

    Button angepasst

--- app/src/main/java/com/example/anton/ma_ced/Settings.java
+++ app/src/main/java/com/example/anton/ma_ced/Settings.java
@@ -1,7 +1,10 @@
 package com.example.anton.ma_ced;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +12,31 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
+        Button sicherheitsebene2 = (Button)findViewById(R.id.sicherheitbtn);
+        sicherheitsebene2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecurityScene.class);
+                //show how to pass information to another activity
+                startIntent.putExtra("com.example.anton.myfirstapp.SOMETHING", "Calculator");
+                startActivity(startIntent);
+            }
+        });
+
+        Button sicherheitsebene = (Button)findViewById(R.id.sicherheitbtn);
+        sicherheitsebene.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), SecurityScene.class);
+                //show how to pass information to another activity
+                startIntent.putExtra("com.example.anton.myfirstapp.SOMETHING", "Calculator");
+                startActivity(startIntent);
+            }
+        });
     }
 }
